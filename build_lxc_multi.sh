414279d77ab015b12ec8270612c1110af9b0ecd4
Anthony Young <sleepsonthefloor@gmail.com>
2011-09-16 00:29:46 -0700

    quiet wget

--- build_lxc_multi.sh
+++ build_lxc_multi.sh
@@ -21,7 +21,7 @@ function run_lxc {
 run_lxc STACKMASTER $HEAD_HOST "ENABLED_SERVICES=g-api,g-reg,key,n-api,n-sch,n-vnc,dash,mysql,rabbit"
 
 # Wait till the head node is up
-while ! wget -O - http://$HEAD_HOST | grep -q username; do
+while ! wget -q -O- http://$HEAD_HOST | grep -q username; do
     echo "Waiting for head node ($HEAD_HOST) to start..."
     sleep 5
 done
