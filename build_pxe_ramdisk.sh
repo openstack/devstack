f2d6c92bb5f196ca8fbb0ed0219988fe5cb48d65
Jesse Andrews <anotherjesse@gmail.com>
2011-09-28 17:50:40 -0700

    missed this when separating out the images

--- build_pxe_ramdisk.sh
+++ build_pxe_ramdisk.sh
@@ -14,7 +14,7 @@ if [ ! -d natty-base ]; then
     debootstrap natty natty-base
     # copy kernel modules...  
     # NOTE(ja): is there a better way to do this?
-    cp -pr /lib/modules/`uname -r` proto/lib/modules
+    cp -pr /lib/modules/`uname -r` natty-base/lib/modules
     cp files/sources.list natty-base/etc/apt/sources.list
     chroot natty-base apt-get update
 fi
