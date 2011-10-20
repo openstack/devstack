199d76e2534b39ea846ca345923b03198dff246c
Scott Moser <smoser@brickies.net>
2011-10-20 12:41:40 -0400

    redirect output of rabbitmq install (LP: #878600)
    as documented in ubuntu bug bug 878600, starting the rabbitmq
    daemon was keeping its output filehandle open, which caused the 'tee'
    to never die, and an ssh connection to never close.

--- stack.sh
+++ stack.sh
@@ -351,7 +351,11 @@ cp $FILES/screenrc ~/.screenrc
 
 if [[ "$ENABLED_SERVICES" =~ "rabbit" ]]; then
     # Install and start rabbitmq-server
-    sudo apt-get install -y -q rabbitmq-server
+    # the temp file is necessary due to LP: #878600
+    tfile=$(mktemp)
+    sudo apt-get install -y -q rabbitmq-server > "$tfile" 2>&1
+    cat "$tfile"
+    rm -f "$tfile"
     # change the rabbit password since the default is "guest"
     sudo rabbitmqctl change_password guest $RABBIT_PASSWORD
 fi
