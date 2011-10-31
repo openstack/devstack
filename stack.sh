d80e380675a3b9bf003d8858a3776981df427f43
Vishvananda Ishaya <vishvananda@gmail.com>
2011-10-31 13:31:19 -0700

    fix warning line to include oneiric as well

--- stack.sh
+++ stack.sh
@@ -23,7 +23,7 @@
 # Warn users who aren't on natty, but allow them to override check and attempt
 # installation with ``FORCE=yes ./stack``
 if ! egrep -q 'natty|oneiric' /etc/lsb-release; then
-    echo "WARNING: this script has only been tested on natty"
+    echo "WARNING: this script has only been tested on natty and oneiric"
     if [[ "$FORCE" != "yes" ]]; then
         echo "If you wish to run this script anyway run with FORCE=yes"
         exit 1
