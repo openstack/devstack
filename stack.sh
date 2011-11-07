7c259cea84b474b76b811f63a4f5274214d96fb8
Anthony Young <sleepsonthefloor@gmail.com>
2011-11-07 13:18:28 -0600

    Use eth0 to determine host ip address

--- stack.sh
+++ stack.sh
@@ -169,9 +169,13 @@ LIBVIRT_TYPE=${LIBVIRT_TYPE:-kvm}
 # cases unless you are working on multi-zone mode.
 SCHEDULER=${SCHEDULER:-nova.scheduler.simple.SimpleScheduler}
 
-# Use the first IP unless an explicit is set by ``HOST_IP`` environment variable
+# Use the eth0 IP unless an explicit is set by ``HOST_IP`` environment variable
 if [ ! -n "$HOST_IP" ]; then
-    HOST_IP=`LC_ALL=C /sbin/ifconfig  | grep -m 1 'inet addr:'| cut -d: -f2 | awk '{print $1}'`
+    HOST_IP=`LC_ALL=C /sbin/ifconfig eth0 | grep -m 1 'inet addr:'| cut -d: -f2 | awk '{print $1}'`
+    if [ "$HOST_IP" = "" ];
+        echo "Could not determine host ip address.  Please specify HOST_IP in your localrc."
+        exit 1
+    fi
 fi
 
 # Service startup timeout
