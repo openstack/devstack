305e3f5524603f700eaeea31586f6a015b5aec29
Anthony Young <sleepsonthefloor@gmail.com>
2011-11-10 14:36:42 -0800

    tweaks to the hacky clean script

--- tools/jenkins/jenkins_home/clean.sh
+++ tools/jenkins/jenkins_home/clean.sh
@@ -3,19 +3,19 @@
 
 set -o errexit
 
-if [ ! "$FORCE" = 1 ]; then
+if [ ! "$FORCE" = "yes" ]; then
     echo "FORCE not set to 1.  Make sure this is something you really want to do.  Exiting."
     exit 1
 fi
 
-exit
 virsh list | cut -d " " -f1 | grep -v "-" | egrep -e "[0-9]" | xargs -n 1 virsh destroy || true
 virsh net-list | grep active | cut -d " " -f1 | xargs -n 1 virsh net-destroy || true
-killall dnsmasq
-rm -rf jobs
+killall dnsmasq || true
+if [ "$CLEAN" = "yes" ]; then
+    rm -rf jobs
+fi
 rm /var/lib/jenkins/jobs
 git checkout -f
 git fetch
 git merge origin/jenkins
 ./build_jenkins.sh
-
