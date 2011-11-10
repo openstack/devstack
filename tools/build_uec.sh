b7661282d10eefe9df260841286f49d04f248c7a
Anthony Young <sleepsonthefloor@gmail.com>
2011-11-10 13:09:25 -0800

    still trying to get pub key working

--- tools/build_uec.sh
+++ tools/build_uec.sh
@@ -191,12 +191,13 @@ EOF
 
 # Setup stack user with our key
 if [ -e ~/.ssh/id_rsa.pub ]; then
+    PUB_KEY=`cat  ~/.ssh/id_rsa.pub`
     cat >> $vm_dir/uec/user-data<<EOF
 mkdir -p /opt/stack
 useradd stack -s /bin/bash -d /opt/stack -G libvirtd || true
 echo stack:pass | chpasswd
 mkdir -p /opt/stack/.ssh
-echo "`cat ~/.ssh/id_rsa.pub`" > /opt/stack/.ssh/authorized_keys
+echo "$PUB_KEY" > /opt/stack/.ssh/authorized_keys
 chown -R stack /opt/stack
 chmod 700 /opt/stack/.ssh
 chmod 600 /opt/stack/.ssh/authorized_keys
