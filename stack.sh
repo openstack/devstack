cc86a9e43a23a1e7f86d978fbea3eb56824c705a
Justin Santa Barbara <justin@fathomdb.com>
2012-02-06 12:15:59 -0800

    Support passing extra args to network create if NETWORK_CREATE_ARGS is defined
    Change-Id: I17b96b74bc7c137589e9af85379c9c6c1bed76c8

--- stack.sh
+++ stack.sh
@@ -1479,7 +1479,7 @@ fi
 # happen after we've started the Quantum service.
 if is_service_enabled mysql; then
     # create a small network
-    $NOVA_DIR/bin/nova-manage network create private $FIXED_RANGE 1 $FIXED_NETWORK_SIZE
+    $NOVA_DIR/bin/nova-manage network create private $FIXED_RANGE 1 $FIXED_NETWORK_SIZE $NETWORK_CREATE_ARGS
 
     if is_service_enabled q-svc; then
         echo "Not creating floating IPs (not supported by QuantumManager)"
