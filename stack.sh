f8be4288836c1dac97871a733401a81f8be93ea5
Chmouel Boudjnah <chmouel@chmouel.com>
2012-06-19 11:01:04 +0000

    Fix swift3 middleware entry point.
    - Fixes bug 1015071.
    
    Change-Id: I969d8826b2a2633488d6eac321e1f52bd47bf7f8

--- stack.sh
+++ stack.sh
@@ -1497,7 +1497,7 @@ admin_user = swift
 admin_password = ${SERVICE_PASSWORD}
 
 [filter:swift3]
-use = egg:swift3#middleware
+use = egg:swift3#swift3
 EOF
 
     cp ${SWIFT_DIR}/etc/swift.conf-sample ${SWIFT_CONFIG_DIR}/swift.conf
