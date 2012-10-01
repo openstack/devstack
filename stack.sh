bc071bcef0bcb726e49f9ccaa2063f58b7eaf96d
Dean Troyer <dtroyer@gmail.com>
2012-10-01 14:06:44 -0500

    Configure PKI cache dirs
    * Configure Cinder, Glance, Keystone, Nova to put cached credentials
      from keystone.auth_token into /var/cache/<service>
    
    It is not obvious to me that having each of these service share a
    credentials cache is a good idea.  It does appear to work but this
    patch takes the conservative approach of putting each service's cache
    in a distinct directory.
    
    More importantly it gets them out of $HOME!
    
    Change-Id: If88088fc287a2f2f4f3e34f6d9be9de3da7ee00d

--- stack.sh
+++ stack.sh
@@ -2042,7 +2042,7 @@ fi
 
 if is_service_enabled g-reg; then
     echo_summary "Uploading images"
-    TOKEN=$(keystone  token-get | grep ' id ' | get_field 2)
+    TOKEN=$(keystone token-get | grep ' id ' | get_field 2)
 
     # Option to upload legacy ami-tty, which works with xenserver
     if [[ -n "$UPLOAD_LEGACY_TTY" ]]; then
