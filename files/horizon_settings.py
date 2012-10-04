1b23d7cd6e49b124f77765bc2e1e324357f8812c
Dean Troyer <dtroyer@gmail.com>
2012-10-04 13:52:22 -0500

    Update horizon config to current example from horizon repo
    includes https://review.openstack.org/14048
    
    Change-Id: I49952dd34408d2e94bcecd9063c21633f4139a99

--- files/horizon_settings.py
+++ files/horizon_settings.py
@@ -1,9 +1,27 @@
 import os
 
+from django.utils.translation import ugettext_lazy as _
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 PROD = False
 USE_SSL = False
+
+# Set SSL proxy settings:
+# For Django 1.4+ pass this header from the proxy after terminating the SSL,
+# and don't forget to strip it from the client's request.
+# For more information see:
+# https://docs.djangoproject.com/en/1.4/ref/settings/#secure-proxy-ssl-header
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
+
+# Specify a regular expression to validate user passwords.
+# HORIZON_CONFIG = {
+#     "password_validator": {
+#         "regex": '.*',
+#         "help_text": _("Your password does not meet the requirements.")
+#     },
+#    'help_url': "http://docs.openstack.org"
+# }
 
 LOCAL_PATH = os.path.dirname(os.path.abspath(__file__))
 
@@ -16,13 +34,23 @@ DATABASES = {
     },
 }
 
-# The default values for these two settings seem to cause issues with apache
-CACHE_BACKEND = 'dummy://'
-SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
-
-# Set a secure and unique SECRET_KEY (the Django default is '')
+# Set custom secret key:
+# You can either set it to a specific value or you can let horizion generate a
+# default secret key that is unique on this machine, e.i. regardless of the
+# amount of Python WSGI workers (if used behind Apache+mod_wsgi): However, there
+# may be situations where you would want to set this explicitly, e.g. when
+# multiple dashboard instances are distributed on different machines (usually
+# behind a load-balancer). Either you have to make sure that a session gets all
+# requests routed to the same dashboard instance or you set the same SECRET_KEY
+# for all of them.
 from horizon.utils import secret_key
 SECRET_KEY = secret_key.generate_or_read_from_file(os.path.join(LOCAL_PATH, '.secret_key_store'))
+
+# We recommend you use memcached for development; otherwise after every reload
+# of the django development server, you will have to login again. To use
+# memcached set CACHE_BACKED to something like 'memcached://127.0.0.1:11211/'
+CACHE_BACKEND = 'dummy://'
+SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
 
 # Send email to the console by default
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
@@ -38,31 +66,55 @@ MAILER_EMAIL_BACKEND = EMAIL_BACKEND
 # EMAIL_HOST_USER = 'djangomail'
 # EMAIL_HOST_PASSWORD = 'top-secret!'
 
+# For multiple regions uncomment this configuration, and add (endpoint, title).
+# AVAILABLE_REGIONS = [
+#     ('http://cluster1.example.com:5000/v2.0', 'cluster1'),
+#     ('http://cluster2.example.com:5000/v2.0', 'cluster2'),
+# ]
+
+OPENSTACK_HOST = "127.0.0.1"
+OPENSTACK_KEYSTONE_URL = "http://%s:5000/v2.0" % OPENSTACK_HOST
+OPENSTACK_KEYSTONE_DEFAULT_ROLE = "Member"
+
+# Disable SSL certificate checks (useful for self-signed certificates):
+# OPENSTACK_SSL_NO_VERIFY = True
+
 HORIZON_CONFIG = {
-    'dashboards': ('nova', 'syspanel', 'settings',),
-    'default_dashboard': 'nova',
+    'dashboards': ('project', 'admin', 'settings',),
+    'default_dashboard': 'project',
 }
 
+# The OPENSTACK_KEYSTONE_BACKEND settings can be used to identify the
+# capabilities of the auth backend for Keystone.
+# If Keystone has been configured to use LDAP as the auth backend then set
+# can_edit_user to False and name to 'ldap'.
+#
 # TODO(tres): Remove these once Keystone has an API to identify auth backend.
 OPENSTACK_KEYSTONE_BACKEND = {
     'name': 'native',
     'can_edit_user': True
 }
 
-OPENSTACK_HOST = "127.0.0.1"
-OPENSTACK_KEYSTONE_URL = "http://%s:5000/v2.0" % OPENSTACK_HOST
-# FIXME: this is only needed until keystone fixes its GET /tenants call
-# so that it doesn't return everything for admins
-OPENSTACK_KEYSTONE_ADMIN_URL = "http://%s:35357/v2.0" % OPENSTACK_HOST
-OPENSTACK_KEYSTONE_DEFAULT_ROLE = "Member"
+OPENSTACK_HYPERVISOR_FEATURES = {
+    'can_set_mount_point': True
+}
+
+# OPENSTACK_ENDPOINT_TYPE specifies the endpoint type to use for the endpoints
+# in the Keystone service catalog. Use this setting when Horizon is running
+# external to the OpenStack environment. The default is 'internalURL'.
+#OPENSTACK_ENDPOINT_TYPE = "publicURL"
+
+# The number of objects (Swift containers/objects or images) to display
+# on a single page before providing a paging element (a "more" link)
+# to paginate results.
+API_RESULT_LIMIT = 1000
+API_RESULT_PAGE_SIZE = 20
 
 SWIFT_PAGINATE_LIMIT = 100
 
-# If you have external monitoring links, eg:
-# EXTERNAL_MONITORING = [
-#     ['Nagios','http://foo.com'],
-#     ['Ganglia','http://bar.com'],
-# ]
+# The timezone of the server. This should correspond with the timezone
+# of your entire OpenStack installation, and hopefully be in UTC.
+TIME_ZONE = "UTC"
 
 #LOGGING = {
 #        'version': 1,
@@ -93,11 +145,19 @@ SWIFT_PAGINATE_LIMIT = 100
 #                'handlers': ['console'],
 #                'propagate': False,
 #            },
+#            'openstack_dashboard': {
+#                'handlers': ['console'],
+#                'propagate': False,
+#            },
 #            'novaclient': {
 #                'handlers': ['console'],
 #                'propagate': False,
 #            },
 #            'keystoneclient': {
+#                'handlers': ['console'],
+#                'propagate': False,
+#            },
+#            'glanceclient': {
 #                'handlers': ['console'],
 #                'propagate': False,
 #            },
