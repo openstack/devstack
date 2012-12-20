e7bca2f86446e773472603b18728ce38c33acde9
Vincent Untz <vuntz@suse.com>
2012-12-20 15:09:20 +0100

    Make opensuse-12.2 a supported distribution
    This has been working well in my tests, and has been confirmed to work
    by others.
    
    I'll of course step up to fix things for openSUSE when needed.
    
    Change-Id: I1ecd345adf975b082aff3a473ab94291b39c8c93

--- stack.sh
+++ stack.sh
@@ -110,7 +110,7 @@ disable_negated_services
 
 # Warn users who aren't on an explicitly supported distro, but allow them to
 # override check and attempt installation with ``FORCE=yes ./stack``
-if [[ ! ${DISTRO} =~ (oneiric|precise|quantal|raring|f16|f17|f18) ]]; then
+if [[ ! ${DISTRO} =~ (oneiric|precise|quantal|raring|f16|f17|f18|opensuse-12.2) ]]; then
     echo "WARNING: this script has not been tested on $DISTRO"
     if [[ "$FORCE" != "yes" ]]; then
         echo "If you wish to run this script anyway run with FORCE=yes"
