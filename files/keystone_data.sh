21ddaac8702ade8f8c6fec558f8185419c46eec3
Eoghan Glynn <eglynn@redhat.com>
2013-03-05 12:40:59 +0000

    Remove trailing slash from ceilometer endpoint URLs
    Fixes bug 1146794
    
    Ensure the URLs configured for the ceilometer endpoint in the keystone
    service catalog do not contain a trailing forward slash.
    
    Otherwise, this leads to an extra forward slash in the URL paths
    constructed by the ceilometer client, which in turn is problematic
    for the v2 API.
    
    Change-Id: I7d457efc6e01e4e955388f52a4907524052f0173

--- files/keystone_data.sh
+++ files/keystone_data.sh
@@ -168,9 +168,9 @@ if [[ "$ENABLED_SERVICES" =~ "ceilometer" ]]; then
         keystone endpoint-create \
             --region RegionOne \
             --service_id $CEILOMETER_SERVICE \
-            --publicurl "http://$SERVICE_HOST:8777/" \
-            --adminurl "http://$SERVICE_HOST:8777/" \
-            --internalurl "http://$SERVICE_HOST:8777/"
+            --publicurl "http://$SERVICE_HOST:8777" \
+            --adminurl "http://$SERVICE_HOST:8777" \
+            --internalurl "http://$SERVICE_HOST:8777"
     fi
 fi
 
