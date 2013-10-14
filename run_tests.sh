e0f4065afdd591d2511a4d8689dacab98392b331
Sean Dague <sdague@linux.vnet.ibm.com>
2013-10-14 17:46:51 -0400

    add a simple run_tests.sh to use bash8
    this gives a simple way to run against all the files that we'd
    want to check with bash8. Currently clocking in at 300+ errors
    (no false pos so far that I see).
    
    Change-Id: Idd83b0bf61029b49bb28ad8b6e6261ecbf927555

---
+++ run_tests.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+#
+# this runs a series of unit tests for devstack to ensure it's functioning
+
+if [[ -n $@ ]]; then
+    FILES=$@
+else
+    LIBS=`find lib -type f | grep -v \.md`
+    SCRIPTS=`find . -type f -name \*\.sh`
+    EXTRA="functions"
+    FILES="$SCRIPTS $LIBS $EXTRA"
+fi
+
+echo "Running bash8..."
+
+./tools/bash8.py $FILES
