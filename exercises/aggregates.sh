75e851a6de99d57eaab3e682b249067cb6065cd0
Sahid Orentino Ferdjaoui <sahid.ferdjaoui@cloudwatt.com>
2013-10-16 08:34:05 +0000

    exercices: aggregates needs to be more flexible
    The actual regex checks a result in python format and
    because of the change in the bug 1132961, Jekins failed.
    
    I have update the regex to work with the old result
    and the new result.
    
    Change-Id: I393e1358f99be5f20d9ac8b3e214355a453ecfcb
    Closes-Bug: 1239726

--- exercises/aggregates.sh
+++ exercises/aggregates.sh
@@ -100,7 +100,7 @@ META_DATA_2_KEY=foo
 META_DATA_3_KEY=bar
 
 #ensure no additional metadata is set
-nova aggregate-details $AGGREGATE_ID | egrep "{u'availability_zone': u'$AGGREGATE_A_ZONE'}|{}"
+nova aggregate-details $AGGREGATE_ID | egrep "\|[{u ]*'availability_zone.+$AGGREGATE_A_ZONE'[ }]*\|"
 
 nova aggregate-set-metadata $AGGREGATE_ID ${META_DATA_1_KEY}=123
 nova aggregate-details $AGGREGATE_ID | grep $META_DATA_1_KEY
@@ -117,7 +117,7 @@ nova aggregate-details $AGGREGATE_ID | grep $META_DATA_3_KEY
 nova aggregate-details $AGGREGATE_ID | grep $META_DATA_2_KEY && die $LINENO "ERROR metadata was not cleared"
 
 nova aggregate-set-metadata $AGGREGATE_ID $META_DATA_3_KEY $META_DATA_1_KEY
-nova aggregate-details $AGGREGATE_ID | egrep "{u'availability_zone': u'$AGGREGATE_A_ZONE'}|{}"
+nova aggregate-details $AGGREGATE_ID | egrep "\|[{u ]*'availability_zone.+$AGGREGATE_A_ZONE'[ }]*\|"
 
 
 # Test aggregate-add/remove-host
