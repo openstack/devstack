2a53051c41902c2715545161c0bf0932a7914227
Frank Borkin <borkinfr@gmail.com>
2013-11-15 08:00:31 +0200

    touch missing files

--- stack.sh
+++ stack.sh
@@ -1013,7 +1013,7 @@ if [ $ENABLE_CONTRAIL ]; then
     repo sync
     python third_party/fetch_packages.py
     cd third_party/thrift-*
-    touch configure.ac
+    touch configure.ac README ChangeLog
     autoreconf --force --install
     cd ../..
     scons
