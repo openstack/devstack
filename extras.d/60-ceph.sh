36f2f024dbeafdc4cbd7a76ed1c96d28fea61fb4
Sébastien Han <sebastien.han@enovance.com>
2014-01-06 18:09:26 +0100

    Implement Ceph backend for Glance / Cinder / Nova
    The new lib installs a full Ceph cluster. It can be managed
    by the service init scripts. Ceph can also be installed in
    standalone without any other components.
    This implementation adds the auto-configuration for
    the following services with Ceph:
    
    * Glance
    * Cinder
    * Cinder backup
    * Nova
    
    To enable Ceph simply add: ENABLED_SERVICES+=,ceph to your localrc.
    If you want to play with the Ceph replication, you can use the
    CEPH_REPLICAS option and set a replica. This replica will be used for
    every pools (Glance, Cinder, Cinder backup and Nova). The size of the
    loopback disk used for Ceph can also be managed thanks to the
    CEPH_LOOPBACK_DISK_SIZE option.
    
    Going further pools, users and PGs are configurable as well. The
    convention is <SERVICE_NAME_IN_CAPITAL>_CEPH_<OPTION> where services are
    GLANCE, CINDER, NOVA, CINDER_BAK. Let's take the example of Cinder:
    
    * CINDER_CEPH_POOL
    * CINDER_CEPH_USER
    * CINDER_CEPH_POOL_PG
    * CINDER_CEPH_POOL_PGP
    
    ** Only works on Ubuntu Trusty, Fedora 19/20 or later **
    
    Change-Id: Ifec850ba8e1e5263234ef428669150c76cfdb6ad
    Implements: blueprint implement-ceph-backend
    Signed-off-by: Sébastien Han <sebastien.han@enovance.com>

---
+++ extras.d/60-ceph.sh
@@ -0,0 +1,44 @@
+# ceph.sh - DevStack extras script to install Ceph
+
+if is_service_enabled ceph; then
+    if [[ "$1" == "source" ]]; then
+        # Initial source
+        source $TOP_DIR/lib/ceph
+    elif [[ "$1" == "stack" && "$2" == "pre-install" ]]; then
+        echo_summary "Installing Ceph"
+        install_ceph
+        echo_summary "Configuring Ceph"
+        configure_ceph
+        # NOTE (leseb): Do everything here because we need to have Ceph started before the main
+        # OpenStack components. Ceph OSD must start here otherwise we can't upload any images.
+        echo_summary "Initializing Ceph"
+        init_ceph
+        start_ceph
+    elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
+        if is_service_enabled glance; then
+            echo_summary "Configuring Glance for Ceph"
+            configure_ceph_glance
+        fi
+        if is_service_enabled nova; then
+            echo_summary "Configuring Nova for Ceph"
+            configure_ceph_nova
+        fi
+        if is_service_enabled cinder; then
+            echo_summary "Configuring Cinder for Ceph"
+            configure_ceph_cinder
+            # NOTE (leseb): the part below is a requirement from Cinder in order to attach volumes
+            # so we should run the following within the if statement.
+            echo_summary "Configuring libvirt secret"
+            import_libvirt_secret_ceph
+        fi
+    fi
+
+    if [[ "$1" == "unstack" ]]; then
+        stop_ceph
+        cleanup_ceph
+    fi
+
+    if [[ "$1" == "clean" ]]; then
+        cleanup_ceph
+    fi
+fi
