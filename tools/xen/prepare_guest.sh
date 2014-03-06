bb1e07859cce688e3beed2c573e9073a72f778fb
Joe Gordon <joe.gordon0@gmail.com>
2014-03-06 09:40:27 -0800

    Don't install vim or locate by default
    Devstack doesn't need vim or locate, if someone wants to use them, they
    can just install them afterwards.
    
    Change-Id: I00f27c20c86d89465e4aefc67ed645a309c09a03

--- tools/xen/prepare_guest.sh
+++ tools/xen/prepare_guest.sh
@@ -73,7 +73,7 @@ EOF
 # Install basics
 apt-get update
 apt-get install -y cracklib-runtime curl wget ssh openssh-server tcpdump ethtool
-apt-get install -y curl wget ssh openssh-server python-pip git vim-nox sudo python-netaddr
+apt-get install -y curl wget ssh openssh-server python-pip git sudo python-netaddr
 pip install xenapi
 
 # Install XenServer guest utilities
