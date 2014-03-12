ea796c0598feeca099d3f57effe4abfb9cb05199
Zhenguo Niu <Niu.ZGlinux@gmail.com>
2014-03-12 16:58:12 +0800

    Add support for django_openstack_auth
    It would be useful for development, reviewing and testing to add
    support for django_openstack_auth to devstack.
    
    This change adds the integration tests to the openstack_auth
    list of test: https://review.openstack.org/#/c/86528/
    
    (Use setup_install instead)
    
    Change-Id: Ifbd336b83f6b2beb23996b599ec820232c13efdd
    Closes-Bug: #1262121
    (cherry picked from commit e385d1e0309a4fc5d414277260702a7c0fff6ad0)

--- stack.sh
+++ stack.sh
@@ -742,6 +742,8 @@ if is_service_enabled nova; then
 fi
 
 if is_service_enabled horizon; then
+    # django openstack_auth
+    install_django_openstack_auth
     # dashboard
     install_horizon
     configure_horizon
