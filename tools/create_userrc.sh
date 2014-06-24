openstack/devstack

commit 4f7bf6963a1e9ae03bc0ae9189874cba561ad62f

Fix getting EC2 and S3 urls in create_userrc
Invalid parsing of openstackclient output was introduced in
I03c17cfc9740c3eb257152c95d9c4f021db2c89e. Here command lines
was changed to get urls with no parsing.

Also default EC2_URL was fixed.

Change-Id: Ic46a9989321cf6310c56edeeb737c603a4f9c1ed

tools/create_userrc.sh
@@ -126,12 +126,12 @@ fi
 
 export -n SERVICE_TOKEN SERVICE_ENDPOINT OS_SERVICE_TOKEN OS_SERVICE_ENDPOINT
 
-EC2_URL=`openstack endpoint show ec2 | grep " ec2.publicURL " | cut -d " " -f4`
+EC2_URL=$(openstack endpoint show -f value -c publicurl ec2)
 if [[ -z $EC2_URL ]]; then
-    EC2_URL=http://localhost:8773/service/Cloud
+    EC2_URL=http://localhost:8773/services/Cloud
 fi
 
-S3_URL=`openstack endpoint show s3 | grep " s3.publicURL " | cut -d " " -f4`
+S3_URL=$(openstack endpoint show -f value -c publicurl s3)
 if [[ -z $S3_URL ]]; then
     S3_URL=http://localhost:3333
 fi

commit bcdce9ea12a5a0613936bbfdc13f43f7f0c13d97

Handle failure of openstack endpoint
Change 4f7bf6963a1e9ae03bc0ae9189874cba561ad62f introduced finding the
endpoint via "openstack endpoint show" only.  Previously that output
was piped through grep, etc. so it handled fell back to a blank
output when the endpoints were not available.

Ignore failures of this command so the blank output is matched

Change-Id: Iae2f09784ffa38efd2f858513bf284c84c32edac

tools/create_userrc.sh
@@ -126,12 +126,12 @@ fi
 
 export -n SERVICE_TOKEN SERVICE_ENDPOINT OS_SERVICE_TOKEN OS_SERVICE_ENDPOINT
 
-EC2_URL=$(openstack endpoint show -f value -c publicurl ec2)
+EC2_URL=$(openstack endpoint show -f value -c publicurl ec2 || true)
 if [[ -z $EC2_URL ]]; then
     EC2_URL=http://localhost:8773/services/Cloud
 fi
 
-S3_URL=$(openstack endpoint show -f value -c publicurl s3)
+S3_URL=$(openstack endpoint show -f value -c publicurl s3 || true)
 if [[ -z $S3_URL ]]; then
     S3_URL=http://localhost:3333
 fi