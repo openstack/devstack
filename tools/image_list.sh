fafb62b93e56b43d20e3dbbee45534f2dcb3d590
Sean Dague <sean@dague.net>
2014-06-30 16:49:30 -0400

    fix trove image addition
    the previous addition to trove was done in a way that it was not
    possible to discover from nodepool. This fixes our ability to do
    that, especially with non-default on services.
    
    Change-Id: I8ca682354e0d4e2eb9cf05f40c6dd783de1c6355

--- tools/image_list.sh
+++ tools/image_list.sh
@@ -10,7 +10,7 @@ source $TOP_DIR/functions
 DRIVERS="openvz ironic libvirt vsphere xenserver dummy"
 
 # Extra variables to trigger getting additional images.
-ENABLED_SERVICES=h-api
+export ENABLED_SERVICES="h-api,tr-api"
 HEAT_FETCHED_TEST_IMAGE="Fedora-i386-20-20131211.1-sda"
 PRECACHE_IMAGES=True
 
