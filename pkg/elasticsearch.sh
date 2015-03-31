dd62293591fd1e822f59754cece645639a4d2d2c
Wayne Okuma <wayne.okuma@hp.com>
2015-03-31 00:28:39 -0700

    Catalog Index Service - glance devstack
    Implements: blueprint catalog-index-service
    
    The changes to lib/glance incorporate the new g-search service.
    The g-search service is optional.
    To enable it add the following line to devstack/local.conf:
    enable_service g-search
    
    In addition to deploying g-search, the changes will also populate a
    search type of keystone service and adds in appropriate endpoints.
    
    Change-Id: I0272d56bc2e50e8174db78bd449f65f60f7f4000

--- pkg/elasticsearch.sh
+++ pkg/elasticsearch.sh
@@ -7,6 +7,8 @@
 TOP_DIR=$(cd $(dirname "$0")/.. && pwd)
 FILES=$TOP_DIR/files
 source $TOP_DIR/functions
+DEST=${DEST:-/opt/stack}
+source $TOP_DIR/lib/infra
 
 # Package source and version, all pkg files are expected to have
 # something like this, as well as a way to override them.
