bf8735ced4e9793ed89676225e2694acbb139354
Matthew Treinish <mtreinish@kortar.org>
2015-10-13 09:51:17 -0400

    Add subunit output for total elapsed time
    This commit adds a success output for the entire devstack run to the
    subunit output. Ideally we wouldn't need this, but because we don't
    have timing data for every single operation performed by devstack we
    need to do this to track the total duration of the devstack run.
    
    To capture failures this commit adds saving a devstack event when we
    trip the exit_trap. This will save a similar result to the stream in
    the successful case, but instead mark it as a failure.
    
    This is backported from master branch to generate subunit result so
    d-g can collect it from the new location.
    
    Depends-On: Icc7df33e4d73ba6322af38fbdf3aea230f2fcf4d
    Depends-On: I97a604ac08797909afc236401281a5d84b98d53f
    Change-Id: I07112dde996c3e2c73f5aafc9b73d33d26374633

--- stack.sh
+++ stack.sh
@@ -42,6 +42,8 @@ if [[ -n "$NOUNSET" ]]; then
     set -o nounset
 fi
 
+# Set start of devstack timestamp
+DEVSTACK_START_TIME=$(date +%s)
 
 # Configuration
 # =============
@@ -457,11 +459,14 @@ function exit_trap {
 
     if [[ $r -ne 0 ]]; then
         echo "Error on exit"
+        generate-subunit $DEVSTACK_START_TIME $SECONDS 'fail' >> ${SUBUNIT_OUTPUT}
         if [[ -z $LOGDIR ]]; then
             $TOP_DIR/tools/worlddump.py
         else
             $TOP_DIR/tools/worlddump.py -d $LOGDIR
         fi
+    else
+        generate-subunit $DEVSTACK_START_TIME $SECONDS >> ${SUBUNIT_OUTPUT}
     fi
 
     exit $r
@@ -681,6 +686,9 @@ source $TOP_DIR/tools/install_prereqs.sh
 if [[ "$OFFLINE" != "True" ]]; then
     PYPI_ALTERNATIVE_URL=${PYPI_ALTERNATIVE_URL:-""} $TOP_DIR/tools/install_pip.sh
 fi
+
+# Install subunit for the subunit output stream
+pip_install -U os-testr
 
 TRACK_DEPENDS=${TRACK_DEPENDS:-False}
 
