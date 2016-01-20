1d27155f650e9b8e4053a201f1f08c494b820de2
Matthew Treinish <mtreinish@kortar.org>
2016-01-19 20:29:46 -0500

    Cap installed pip to be < 8
    Pip 8 just release which made uninstalling distutils installed packages
    fatal. This was previously a deprecation warning and is now causing all
    dsvm jobs to fail.
    
    Depends-On: I511d216d9d8619c7cf919c482aaead4e833cdaac
    Change-Id: I22f5c3af1adf96cfbd5747122f915a82e947843a

--- tools/install_pip.sh
+++ tools/install_pip.sh
@@ -77,9 +77,9 @@ function install_get_pip {
             die $LINENO "Download of get-pip.py failed"
         touch $LOCAL_PIP.downloaded
     fi
-    sudo -H -E python $LOCAL_PIP
+    sudo -H -E python $LOCAL_PIP -c $TOOLS_DIR/cap-pip.txt
     if python3_enabled; then
-        sudo -H -E python${PYTHON3_VERSION} $LOCAL_PIP
+        sudo -H -E python${PYTHON3_VERSION} $LOCAL_PIP -c $TOOLS_DIR/cap-pip.txt
     fi
 }
 
