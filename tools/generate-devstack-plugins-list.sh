8ce3faf4e73297397cc84e9b64c38eb876d6c7d1
Clint Adams <clint@gcfm.net>
2016-02-26 08:18:13 -0700

    Remove filesystem-based code path
    This removes the fs-based git code path for detecting devstack plugins
    as requested in I2c5c9282a8ad80014cad171a4dfbdc8f26044cd1
    
    Change-Id: I6d1567c2545b866c433381d19587beb08c281c53

--- tools/generate-devstack-plugins-list.sh
+++ tools/generate-devstack-plugins-list.sh
@@ -43,38 +43,14 @@ declare -A plugins
 
 test -r data/devstack-plugins-registry.header && cat data/devstack-plugins-registry.header
 
-if test -n "$git_dir"; then
-    pushd ${git_dir} >/dev/null
-    for i in *; do
-        pushd ${i} >/dev/null
-        if output="$(git log --diff-filter=A --format='%cd' --date=short -1 -- devstack/plugin.sh)"; then
-            test -n "$output" && plugins[$i]=${output}
-        fi
-        popd >/dev/null
-    done
-    popd >/dev/null
+sorted_plugins=$(python tools/generate-devstack-plugins-list.py)
 
-    sorted_plugins=( $(for k in "${!plugins[@]}"; do echo "$k"; done | sort))
-
-    for k in "${sorted_plugins[@]}"; do
-        project=${k:0:18}
-        giturl="git://git.openstack.org/openstack/${k:0:26}"
-        pdate="${plugins[$k]}"
-        printf "|%-18s|%-60s (%-10s)|\n" "${project}" "${giturl}" "${pdate}"
-        printf "+------------------+-------------------------------------------------------------------------+\n"
-    done
-
-else
-    sorted_plugins=$(python tools/generate-devstack-plugins-list.py)
-
-    for k in ${sorted_plugins}; do
-        project=${k:0:18}
-        giturl="git://git.openstack.org/openstack/${k:0:26}"
-        printf "|%-18s|%-73s|\n" "${project}" "${giturl}"
-        printf "+------------------+-------------------------------------------------------------------------+\n"
-    done
-
-fi
+for k in ${sorted_plugins}; do
+    project=${k:0:18}
+    giturl="git://git.openstack.org/openstack/${k:0:26}"
+    printf "|%-18s|%-73s|\n" "${project}" "${giturl}"
+    printf "+------------------+-------------------------------------------------------------------------+\n"
+done
 
 test -r data/devstack-plugins-registry.footer && cat data/devstack-plugins-registry.footer
 ) > doc/source/plugin-registry.rst
