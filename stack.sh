9162608d6e1d13a64dd387486faa221c694ba913
Ian Wienand <iwienand@redhat.com>
2016-08-04 15:17:38 +1000

    Revert "stackrc set the LC_ALL to C"
    This reverts commit f327b1e1196eacf25e7c4c9e3a7ad30c53bb961c.
    
    The problem being addressed in the original commit was that
    rejoin-stack.sh would run with the user's locale, instead of C that
    was set in stack.sh
    
    We overlooked that this gets pulled in by openrc, so it is overriding
    the user's locale when using clients, etc.
    
    rejoin-stack.sh was removed in
    I2f61bb69cc110468a91dcaa4ee7653ede7048467 so we don't have to worry
    about that part.  A revert to not touch the user's locale seems
    appropriate.
    
    Change-Id: I7c858bb92ce7ba5b5d323bf3ad6776100026c7a2
    Closes-Bug: #1608687

--- stack.sh
+++ stack.sh
@@ -27,6 +27,13 @@ set -o xtrace
 # Make sure custom grep options don't get in the way
 unset GREP_OPTIONS
 
+# Sanitize language settings to avoid commands bailing out
+# with "unsupported locale setting" errors.
+unset LANG
+unset LANGUAGE
+LC_ALL=C
+export LC_ALL
+
 # Make sure umask is sane
 umask 022
 
