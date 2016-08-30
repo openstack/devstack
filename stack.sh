0bf25506b03c78dd809e3638cabcf1fc11e9f1a0
melanie witt <melwittt@gmail.com>
2016-08-30 22:14:04 +0000

    Move RABBIT_USERID to lib/rpc_backend
    This moves setting of RABBIT_USERID from stack.sh to lib/rpc_backend
    so it may be used in grenade runs, which don't have the defaulted
    value from stack.sh. The RABBIT_USERID is needed in order to call
    get_transport_url in lib/rpc_backend.
    
    Change-Id: I6f211e9102f79418f9f94a15784f91c4150ab8a7

--- stack.sh
+++ stack.sh
@@ -664,7 +664,6 @@ initialize_database_backends && echo "Using $DATABASE_TYPE database backend" || 
 # Rabbit connection info
 # In multi node DevStack, second node needs ``RABBIT_USERID``, but rabbit
 # isn't enabled.
-RABBIT_USERID=${RABBIT_USERID:-stackrabbit}
 if is_service_enabled rabbit; then
     RABBIT_HOST=${RABBIT_HOST:-$SERVICE_HOST}
     read_password RABBIT_PASSWORD "ENTER A PASSWORD TO USE FOR RABBIT."
