5713497aa5819f240050fee05d047c480845a8cd
Matt Riedemann <mriedem@us.ibm.com>
2017-01-20 09:01:49 -0500

    Add discover_hosts.sh script
    This adds a simple script to run the
    'nova-manage cell_v2 discover_hosts'
    command which will be used by
    devstack-gate to discover the compute
    hosts after devstack is fully setup.
    
    This allows us to manage the branches
    where this can run from devstack rather
    than require branch logic in devstack-gate.
    
    Change-Id: Icc595d60de373471aa7ee8fb9f3a81fc12d80438
    Depends-On: I4823737246a8e9cc4eaebf67ff6bdba8bf42ab29

---
+++ tools/discover_hosts.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# **discover_hosts.sh**
+
+# This is just a very simple script to run the
+# "nova-manage cell_v2 discover_hosts" command
+# which is needed to discover compute nodes and
+# register them with a parent cell in Nova.
+# This assumes that /etc/nova/nova.conf exists
+# and has the following entries filled in:
+#
+# [api_database]
+# connection = This is the URL to the nova_api database
+#
+# In other words this should be run on the primary
+# (API) node in a multi-node setup.
+
+nova-manage cell_v2 discover_hosts --verbose
