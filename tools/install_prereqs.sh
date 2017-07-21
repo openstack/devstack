d325875508e7d35d6dd62302d852e83815be2278
Dirk Mueller <dirk@dmllr.de>
2017-07-21 08:19:16 +0200

    Resolve openSUSE devstack failures
    This update resolves multiple issues with python-cryptography
    causing keystone server and nova deployment to fail.
    
    This is a temporary workaround until I196f025dbf1a9ac297946b8165620676645f7210
    has landed and the extraneous dependency on python-cryptography (the
    package) has been removed.
    
    Change-Id: Ifb29b9089197c0429a5fc1cd08a25d2095d481f1

--- tools/install_prereqs.sh
+++ tools/install_prereqs.sh
@@ -88,6 +88,22 @@ else
     export PYTHON=$(which python 2>/dev/null)
 fi
 
+if is_suse; then
+    # novnc has an extraneous dependency on pyOpenSSL, which causes symbol conflicts
+    # in the bundled libssl of python-cryptography. when both are loaded into the same
+    # process, they start hanging or segfaulting.
+    install_package novnc
+    # deinstall the extra but irrelevant dependencies
+    sudo rpm -e --nodeps python-cffi python-cryptography python-pyOpenSSL
+    # reinstall cffi which got overwriten by the package.
+    sudo pip install -I cffi
+    # now reinstall cryptography from source, in order to rebuilt it against the
+    # system libssl rather than the bundled openSSL 1.1, which segfaults when combined
+    # with the system provided (which libpython links against) openSSL 1.0
+    sudo pip install cryptography --no-binary :all:
+fi
+
+
 # Mark end of run
 # ---------------
 
