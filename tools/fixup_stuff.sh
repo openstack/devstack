10f44098825d1d13452117dc5ee31e3e895b495f
Colleen Murphy <colleen@gazlene.net>
2019-02-28 23:44:14 +0100

    Add hack to fix ply installation on opensuse
    The python3-ply package is indirectly a dependency of dhcp-client, which
    is not exactly an optional package. Pip >=10 refuses to install ply from
    global-requirements with this distro package is installed, so our only
    option is to remove it manually.
    
    Change-Id: I377fdd4a581eb4b6275584d92cafc0b783fe3b84

--- tools/fixup_stuff.sh
+++ tools/fixup_stuff.sh
@@ -228,6 +228,14 @@ function fixup_suse {
         sudo systemctl disable apparmor
         sudo /usr/sbin/aa-teardown
     fi
+
+    # Since pip10, pip will refuse to uninstall files from packages
+    # that were created with distutils (rather than more modern
+    # setuptools).  This is because it technically doesn't have a
+    # manifest of what to remove.  However, in most cases, simply
+    # overwriting works.  So this hacks around those packages that
+    # have been dragged in by some other system dependency
+    sudo rm -rf /usr/lib/python3.6/site-packages/ply-*.egg-info
 }
 
 # The version of pip(1.5.4) supported by python-virtualenv(1.11.4) has
