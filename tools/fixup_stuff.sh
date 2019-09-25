0e6774e0e7426929750d37aa7f2bf16225b78c80
Colleen Murphy <colleen.murphy@suse.de>
2019-09-25 12:51:23 -0700

    Fix six package on opensuse for pip 10
    openSUE Leap 15.0 and 15.1 both provide python3-six version 1.11.0.
    Since version 1.12.0 was released, pip>=10 recognizes the version
    difference and tries to uninstall the distro-provided version and fails.
    This change adds another hack to remove the egg-info file for the six
    library so that pip can manage it directly. We also have to wait to
    install os-testr until after the fixup has happened since trying to
    install it triggers the issue.
    
    Depends-on: https://review.opendev.org/715175
    
    Change-Id: I4649abe06b5893a5251bfcdd4234abccde6ceda2
    (cherry picked from commit 6eb2c5990d738612c4cec34daeb673092303a50a)

--- tools/fixup_stuff.sh
+++ tools/fixup_stuff.sh
@@ -221,6 +221,15 @@ function fixup_suse {
     # See https://bugzilla.suse.com/show_bug.cgi?id=1154871
     # May be removed once a new opensuse-15 image is available in nodepool
     sudo zypper up -y p11-kit ca-certificates-mozilla
+
+    # Since pip10, pip will refuse to uninstall files from packages
+    # that were created with distutils (rather than more modern
+    # setuptools).  This is because it technically doesn't have a
+    # manifest of what to remove.  However, in most cases, simply
+    # overwriting works.  So this hacks around those packages that
+    # have been dragged in by some other system dependency
+    sudo rm -rf /usr/lib/python3.6/site-packages/ply-*.egg-info
+    sudo rm -rf /usr/lib/python3.6/site-packages/six-*.egg-info
 }
 
 # The version of pip(1.5.4) supported by python-virtualenv(1.11.4) has
