3cd41019b048349b42ec62d5602beb89bed9e975
Ian Wienand <iwienand@redhat.com>
2020-04-16 13:06:07 +1000

    lib/tls: use python3 to run inline script
    We only need to run this fixup for the active python now we are 3 only.
    
    Change-Id: I7616e5ee5693b2890fb7f6bd9052890a82904c22

--- stack.sh
+++ stack.sh
@@ -968,9 +968,6 @@ fi
 
 if is_service_enabled tls-proxy; then
     fix_system_ca_bundle_path
-    if python3_enabled ; then
-        fix_system_ca_bundle_path python3
-    fi
 fi
 
 # Extras Install
