c7791301be5e19ec7a84dffbf62c40e805483f43
melanie witt <melwittt@gmail.com>
2021-10-01 17:24:10 +0000

    Enable oslo.limit to be installed from git repo
    oslo.limit isn't currently in the list of libraries that can be
    installed from a git repo via LIBS_FROM_GIT.
    
    This adds oslo.limit to enable integrated testing against unmerged
    oslo.limit changes.
    
    Change-Id: I26cc567fdf4c84014040ae586bbb029b8de7a236

--- tests/test_libs_from_pypi.sh
+++ tests/test_libs_from_pypi.sh
@@ -45,6 +45,7 @@ ALL_LIBS+=" oslo.cache oslo.reports osprofiler cursive"
 ALL_LIBS+=" keystoneauth ironic-lib neutron-lib oslo.privsep"
 ALL_LIBS+=" diskimage-builder os-vif python-brick-cinderclient-ext"
 ALL_LIBS+=" castellan python-barbicanclient ovsdbapp os-ken os-resource-classes"
+ALL_LIBS+=" oslo.limit"
 
 # Generate the above list with
 # echo ${!GITREPO[@]}
